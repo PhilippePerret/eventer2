2864d6853b55a556bf8e4b6a415b0995377eaefd
PhilippePerret <philippe.perret@yahoo.fr>
2026-05-25 15:52:43 +0200

    Arrangement encore des données de base (non fonctionnelles encore)

--- public/classes/repositories/ProjectRepository.js
+++ public/classes/repositories/ProjectRepository.js
@@ -1,12 +1,13 @@
 export default class ProjectRepository {
 
   async all() {
-    return [
-      {
-        id: '__e2e__',
-        title: '__e2e__'
-      }
-    ]
+    const response = await fetch('/projects')
+
+    if (!response.ok) {
+      throw new Error(`Impossible de charger les projets : HTTP ${response.status}`)
+    }
+
+    return await response.json()
   }
 
 }
